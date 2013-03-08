f3f15e2429bbe3802e7adea58c9ab9252daa8c68
Ian Page Hands <iphands@gmail.com>
2013-03-08 14:11:46 -0500

    NA: jslinting, one issue remains jslint thinks the Markdown() function should be lower case

--- js/jquery.meltdown.js
+++ js/jquery.meltdown.js
@@ -1,275 +1,289 @@
+/*global jQuery, console, Markdown*/
+
 /*
  * Meltdown (Markup Extra Live Toolbox)
  * Version: 0.1 (13-FEB-2013)
  * Requires: jQuery v1.7.2 or later
  */
-;(function($) {
 
-var ver = '0.1';
-var name = 'meltdown';
+(function (jQuery) {
+	'use strict';
 
-$.fn.meltdown = function(userOptions) {
-    return this.each(function() {
-		var defaults = $.fn.meltdown.defaults;
-        var opts = jQuery.extend(true, {}, defaults, userOptions);
-		opts.hasEffects = typeof jQuery.ui != 'undefined';
+	var ver, name;
+	ver = '0.1';
+	name = 'meltdown';
 
-		var thees = jQuery(this);
-		thees.wrap('<div class="' + name + '_wrap" />');
-		thees.before('<div><div style="display: none;" class="' + name + '_preview-wrap"><span class="' + name + '_preview-header">Preview Area</span><div class="' + name + '_preview"></div></div></div><div class="meltdown_bar"><ul class="' + name + '_controls"></ul></div>');
 
-		var wrap = thees.parent();
-		var previewWrap = wrap.children(':eq(0)').children(':eq(0)'); /* wrapper for the preview area, but not where the updated content goes */
-		var preview = previewWrap.children(':eq(1)'); /* preview area where updates happen */
-		var bar = wrap.children(':eq(1)');
-		var controls = bar.children().first();
+	function update(previewArea, input) {
+		previewArea.html(Markdown(input.val()));
+	}
 
-		buildControls(opts, thees, controls);
-		controls.append(getPreivewControl(opts, thees, previewWrap));
-
-		wrap.width(thees.outerWidth());
-		preview.height(thees.outerHeight());
-
-		thees.on('keyup', function(event) {
-			if (previewWrap.is(':visible')) {
-				update(preview, thees);
-			}
-		});
-
-    });
-};
-
-function addEventHandler(thees, example, control) {
-	control.click(function(e) {
-
-		if (typeof jQuery.surroundSelectedText !== undefined) {
-			if (example.type == "wrap") {
-				thees.surroundSelectedText(example.before, example.after, true);
-			} else {
-				selection = thees.getSelection();
-				if (selection.length  === 0) {
-					thees.replaceSelectedText(example.markdown);
+	function addEventHandler(thees, example, control) {
+		var selection;
+		control.click(function (e) {
+			if (typeof jQuery.surroundSelectedText !== undefined) {
+				if (example.type === "wrap") {
+					thees.surroundSelectedText(example.before, example.after, true);
+				} else {
+					selection = thees.getSelection();
+					if (selection.length  === 0) {
+						thees.replaceSelectedText(example.markdown);
+					}
 				}
+			} else {
+				console.log('Failed to load surroundSelectedText');
+				thees.val(example.markdown + "\n\n\n" + thees.val());
 			}
-		} else {
-			console.log('Failed to load surroundSelectedText');
-			thees.val(example.markdown + "\n\n\n" + thees.val());
+			thees.keyup();
+		});
+	}
+
+	function buildControls(opts, thees, controls) {
+		var controlList, example, control, tuple, t, groupClass, group, outer, tmpThis;
+		controlList = [];
+
+		for (example in opts.examples) {
+			if (opts.examples.hasOwnProperty(example)) {
+				example = opts.examples[example];
+
+				control = jQuery('<li><span>' + example.label + '</span></li>');
+				control.addClass(name + '_control');
+				if (typeof example.styleClass !== undefined) {
+					control.addClass(example.styleClass);
+				}
+
+				control.children(":first").attr('title', example.altText);
+				addEventHandler(thees, example, control);
+
+				tuple = {};
+				tuple.example = example;
+				tuple.control = control;
+				controlList.push(tuple);
+			}
 		}
 
+		function addClickHandler(element, outer) {
+			outer.on('click', function () {
+				element.siblings('li').removeClass(name + '_controlgroup-open').children('ul').hide();
+				element.toggleClass(name + '_controlgroup-open').children('ul').toggle();
+			});
+		}
 
-		thees.keyup();
-	});
-}
+		for (t in controlList) {
+			if (controlList.hasOwnProperty(t)) {
+				t = controlList[t];
+				if (t.example.group && t.example.groupLabel) {
+					groupClass = name + "_controlgroup-" + t.example.group;
+					group = controls.find("ul." + groupClass);
+					outer = jQuery('<li />');
+					if (group.length === 0) {
+						group = jQuery('<ul style="display: none;" />');
+						group.addClass(name + '_controlgroup-dropdown ' + groupClass);
+						outer.addClass(name + '_controlgroup ' + groupClass);
+						outer.append('<span>' + t.example.groupLabel + ' <i class="meltdown-icon-caret-down"></i></span><b></b>');
+						outer.append(group);
+						controls.append(outer);
+					}
+					group.append(t.control);
+					addClickHandler(thees, outer);
+				} else {
+					controls.append(t.control);
+				}
+			}
+		}
+	}
 
-function buildControls(opts, thees, controls) {
-	var controlList = [];
-
-	for (var example in opts.examples) {
-		example = opts.examples[example];
-
+	function getAddExampleControl(options, thees, previewArea, example) {
 		var control = jQuery('<li><span>' + example.label + '</span></li>');
 		control.addClass(name + '_control');
 		if (typeof example.styleClass !== undefined) {
 			control.addClass(example.styleClass);
 		}
-
 		control.children(":first").attr('title', example.altText);
-		addEventHandler(thees, example, control);
-
-		var tuple = {};
-		tuple.example = example;
-		tuple.control = control;
-		controlList.push(tuple);
+		control.on('click', function () {
+			thees.val(example.markdown + "\n\n\n" + thees.val());
+			thees.keyup();
+		});
+		return control;
 	}
 
-	for (var t in controlList ) {
-		t = controlList[t];
-		if (t.example.group && t.example.groupLabel) {
-			var groupClass = name + "_controlgroup-" + t.example.group;
-			var group = controls.find("ul." + groupClass);
-			var outer = jQuery('<li />');
-			
-			if (group.length === 0) {
-				group = jQuery('<ul style="display: none;" />');
-				group.addClass(name + '_controlgroup-dropdown ' + groupClass);
-				outer.addClass(name + '_controlgroup ' + groupClass);
-				outer.append('<span>'+t.example.groupLabel+' <i class="meltdown-icon-caret-down"></i></span><b></b>');
-				outer.append(group);
-				controls.append(outer);
-			}
-			group.append(t.control);
-			
-			outer.on('click', function() {
-				$(this).siblings('li').removeClass(name + '_controlgroup-open').children('ul').hide();
-				$(this).toggleClass(name + '_controlgroup-open').children('ul').toggle();
-			});
-			
-		} else {
-			controls.append(t.control);
-		}
-	}
-}
-
-function getAddExampleControl(options, thees, previewArea, example) {
-	var control = jQuery('<li><span>' + example.label + '</span></li>');
-	control.addClass(name + '_control');
-	if (typeof example.styleClass !== undefined) {
-		control.addClass(example.styleClass);
-	}
-	control.children(":first").attr('title', example.altText);
-	control.on('click', function() {
-		thees.val(example.markdown + "\n\n\n" + thees.val());
-		thees.keyup();
-	});
-	return control;
-}
-
-function getPreivewControl(options, thees, previewArea) {
-	var control = jQuery('<li class="' + name + '_control ' + name + '_control-preview"><span title="Show preview">Show preview</span></li>');
-	control.on('click', function() {
-		if (! previewArea.is(':visible')) {
-			if (options.hasEffects) {
-				previewArea.slideToggle(options.previewTimeout);
+	function getPreivewControl(options, thees, previewArea) {
+		var control = jQuery('<li class="' + name + '_control ' + name + '_control-preview"><span title="Show preview">Show preview</span></li>');
+		control.on('click', function () {
+			if (!previewArea.is(':visible')) {
+				if (options.hasEffects) {
+					previewArea.slideToggle(options.previewTimeout);
+				} else {
+					previewArea.fadeIn();
+				}
+				previewArea.addClass(name + 'visible');
+				control.children(':eq(0)').text('Hide preview');
+				control.addClass(name + '_preview-showing');
+				update(previewArea.children(':eq(1)'), thees);
 			} else {
-				previewArea.fadeIn();
+				if (options.hasEffects) {
+					previewArea.slideToggle(options.previewTimeout);
+				} else {
+					previewArea.fadeOut();
+				}
+				previewArea.removeClass(name + 'visible');
+				control.removeClass(name + '_preview-showing');
+				control.children(':eq(0)').text('Show preview');
 			}
-			previewArea.addClass(name + 'visible');
-			control.children(':eq(0)').text('Hide preview');
-			control.addClass(name + '_preview-showing');
-			update(previewArea.children(':eq(1)'), thees);
-		} else {
-			if (options.hasEffects) {
-				previewArea.slideToggle(options.previewTimeout);
-			} else {
-				previewArea.fadeOut();
+		});
+		return control;
+	}
+
+	function getExamples() {
+		var key, examples, pounds, i, j;
+		examples = {
+			bold: {
+				label: "B",
+				altText: "Bold",
+				before: "**",
+				after: "**",
+				type: "wrap"
+			},
+			italics: {
+				label: "I",
+				altText: "Italics",
+				before: "*",
+				after: "*",
+				type: "wrap"
+			},
+			ul: {
+				label: "UL",
+				altText: "Unordered List",
+				markdown: "* Item\n* Item\n"
+			},
+			ol: {
+				label: "OL",
+				altText: "Ordered List",
+				markdown: "1. Item 1\n2. Item 2\n3. Item 3\n\n"
+			},
+			table: {
+				label: "Table",
+				altText: "Table",
+				markdown: "First Header  | Second Header\n------------- | -------------\nContent Cell  | Content Cell\nContent Cell  | Content Cell\n\n"
 			}
-			previewArea.removeClass(name + 'visible');
-			control.removeClass(name + '_preview-showing');
-			control.children(':eq(0)').text('Show preview');
-		}
-	});
-	return control;
-}
-
-function getExamples() {
-	var examples = {
-		bold: {
-			label: "B",
-			altText: "Bold",
-			before: "**",
-			after: "**",
-			type: "wrap"
-		},
-		italics: {
-			label: "I",
-			altText: "Italics",
-			before: "*",
-			after: "*",
-			type: "wrap"
-		},
-		ul: {
-			label: "UL",
-			altText: "Unordered List",
-			markdown: "* Item\n* Item\n"
-		},
-		ol: {
-			label: "OL",
-			altText: "Ordered List",
-			markdown: "1. Item 1\n2. Item 2\n3. Item 3\n\n"
-		},
-		table: {
-			label: "Table",
-			altText: "Table",
-			markdown: "First Header  | Second Header\n------------- | -------------\nContent Cell  | Content Cell\nContent Cell  | Content Cell\n\n"
-		}
-	};
-
-	for (var i = 1; i <= 6; i += 1) {
-		var pounds = "";
-		for (var j = 1; j <= i; j += 1) {
-			pounds = pounds + "#";
-		}
-		examples['h'+i] = {
-			group: "h",
-			groupLabel: "Headers",
-			label: "H"+i,
-			altText: "Header "+i,
-			before: pounds,
-			after: "",
-			type: "wrap"
 		};
-	}
 
-	examples['link'] = {
+		for (i = 1; i <= 6; i += 1) {
+			pounds = "";
+			for (j = 1; j <= i; j += 1) {
+				pounds = pounds + "#";
+			}
+			examples['h' + i] = {
+				group: "h",
+				groupLabel: "Headers",
+				label: "H" + i,
+				altText: "Header " + i,
+				before: pounds,
+				after: "",
+				type: "wrap"
+			};
+		}
+
+		examples.link = {
 			label: "Link",
 			group: "kitchenSink",
 			groupLabel: "Kitchen Sink",
 			altText: "Link",
 			markdown: "[Example link](http://example.com/ \"Link title\")"
-	};
-	
-	examples['img'] = {
+		};
+
+		examples.img = {
 			label: "Image",
 			group: "kitchenSink",
 			groupLabel: "Kitchen Sink",
 			altText: "Image",
 			markdown: "![Alt text](http://image_url)"
-	};
-	
-	examples['blockquote'] = {
-		label: "Blockquote",
-		group: "kitchenSink",
-		groupLabel: "Kitchen Sink",
-		altText: "Blockquote",
-		markdown: "> Example text"
-	};
-	
-	examples['codeblock'] = {
+		};
+
+		examples.blockquote = {
+			label: "Blockquote",
+			group: "kitchenSink",
+			groupLabel: "Kitchen Sink",
+			altText: "Blockquote",
+			markdown: "> Example text"
+		};
+
+		examples.codeblock = {
 			label: "Code Block",
 			group: "kitchenSink",
 			groupLabel: "Kitchen Sink",
 			altText: "Code Block",
 			markdown: "~~~\nExample code\n~~~"
-	};
-	
-	examples['code'] = {
+		};
+
+		examples.code = {
 			label: "Code",
 			group: "kitchenSink",
 			groupLabel: "Kitchen Sink",
 			altText: "Inline Code",
 			markdown: "`Example code`"
-	};
-	
-	examples['footnote'] = {
+		};
+
+		examples.footnote = {
 			label: "Footnote",
 			group: "kitchenSink",
 			groupLabel: "Kitchen Sink",
 			altText: "Footnote",
 			markdown: "[^1]\n\n[^1]:Example footnote"
-	};
-	
-	examples['hr'] = {
-		label: "HR",
-		group: "kitchenSink",
-		groupLabel: "Kitchen Sink",
-		altText: "Horizontal Rule",
-		markdown: "----------"
-	};
+		};
 
-	for (key in examples) {
-		examples[key].styleClass = name + "_control-" + key;
+		examples.hr = {
+			label: "HR",
+			group: "kitchenSink",
+			groupLabel: "Kitchen Sink",
+			altText: "Horizontal Rule",
+			markdown: "----------"
+		};
+
+		for (key in examples) {
+			if (examples.hasOwnProperty(key)) {
+				examples[key].styleClass = name + "_control-" + key;
+			}
+		}
+
+		return examples;
 	}
 
-	return examples;
-}
+	jQuery.fn.meltdown = function (userOptions) {
+		return this.each(function () {
+			var defaults, opts, thees, wrap, previewWrap, preview, bar, controls;
+			defaults = jQuery.fn.meltdown.defaults;
+			opts = jQuery.extend(true, {}, defaults, userOptions);
+			opts.hasEffects = typeof jQuery.ui !== 'undefined';
 
-function update(previewArea, input) {
-	previewArea.html(Markdown(input.val()));
-}
+			thees = jQuery(this);
+			thees.wrap('<div class="' + name + '_wrap" />');
+			thees.before('<div><div style="display: none;" class="' + name + '_preview-wrap"><span class="' + name + '_preview-header">Preview Area</span><div class="' + name + '_preview"></div></div></div><div class="meltdown_bar"><ul class="' + name + '_controls"></ul></div>');
 
-$.fn.meltdown.defaults = {
-	examples: getExamples(),
-	previewTimeout: 400
-};
+			wrap = thees.parent();
+			previewWrap = wrap.children(':eq(0)').children(':eq(0)'); /* wrapper for the preview area, but not where the updated content goes */
+			preview = previewWrap.children(':eq(1)'); /* preview area where updates happen */
+			bar = wrap.children(':eq(1)');
+			controls = bar.children().first();
 
-})(jQuery);
+			buildControls(opts, thees, controls);
+			controls.append(getPreivewControl(opts, thees, previewWrap));
+
+			wrap.width(thees.outerWidth());
+			preview.height(thees.outerHeight());
+
+			thees.on('keyup', function (event) {
+				if (previewWrap.is(':visible')) {
+					update(preview, thees);
+				}
+			});
+
+		});
+	};
+
+	jQuery.fn.meltdown.defaults = {
+		examples: getExamples(),
+		previewTimeout: 400
+	};
+
+}(jQuery));
